9c7890703f2840fcc5d8fb39fdd83dcc29e75c93
brothersw <tylerofawesomeness@gmail.com>
2025-04-17 14:27:23 -0400

    fix: probability

--- util/probability.py
+++ util/probability.py
@@ -2,31 +2,30 @@ from game.card import Suit
 from game.state import State
 
 # returns the expected value to get this suit from the draw pile
-def expected_value(suit: Suit, state: State) -> float:
-    # count the number of cards in the deck that are this suit
+def expected_value(suit: Suit, state: State, seeHand: bool) -> float:
+    # count the number of seen_cards in the deck that are this suit
     count = 0
-    cards = 0
+    seen_cards = 0
     total_count = 95 # 2 + 3 + ... + 10 + 10 + 10 + 10 + 11
-    total_cards = 13 # 52 / 4
+    total_cards = 52
 
     for card in state.discard:
+        seen_cards += 1
         if card.suit == suit:
             count += card.val
-            cards += 1
     
-    if total_cards - cards == 0:
-        return 0.0
-    
-    for hand in state.hands:
+    for i, hand in enumerate(state.hands):
         for card in hand:
-            if card.suit == suit:
-                if card.visible:
+            if card.visible or (seeHand and i == state.turn):
+                seen_cards += 1
+                if card.suit == suit:
                     count += card.val
-                    cards += 1
     
-    # TODO: fix me, this isn't total expected value from the deck (doesn't account for what other cards were seen)
-    return (total_count - count) / (total_cards - cards)
+    if seen_cards == 52:
+        return 0
+
+    return (total_count - count) / (total_cards - seen_cards)
 
 # returns a list of the expected value for each suit in the order of the Suit enum
-def expected_values(state: State) -> list[float]:
-    return [expected_value(suit, state) for suit in Suit]
+def expected_values(state: State, see_hand: bool = False) -> list[float]:
+    return [expected_value(suit, state, see_hand) for suit in Suit]
